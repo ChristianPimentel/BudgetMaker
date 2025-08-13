8688e659facd77eaf8d2b6959d69b6f0a16060d4
Christian Rios <criss2l2@gmail.com>
2025-08-13 19:39:32 +0000

    When using the print button , the report will be everything including th

--- src/components/dashboard.tsx
+++ src/components/dashboard.tsx
@@ -85,7 +85,7 @@ export default function Dashboard() {
         </div>
       </header>
 
-      <main className="flex-1 overflow-auto p-4 md:p-6 lg:p-8">
+      <main className="flex-1 p-4 md:p-6 lg:p-8 print:p-0">
         <div className="max-w-7xl mx-auto space-y-8">
           <BudgetOverview />
           <ReceiptsList onEditReceipt={handleEditReceipt} />
